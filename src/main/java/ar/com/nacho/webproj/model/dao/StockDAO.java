b8dd905b037ee4a276e90aed3f916358c476752a
nacho <j.ignacioquintero@gmail.com>
2020-06-19 00:58:19 -0300

    borrado

--- src/main/java/ar/com/nacho/webproj/model/dao/StockDAO.java
+++ src/main/java/ar/com/nacho/webproj/model/dao/StockDAO.java
@@ -20,52 +20,7 @@ import java.util.ArrayList;
  * @author nacho
  */
 public class StockDAO {
-    /*
-  public ArrayList<Stock> getListado(){
       
-      
-      
-      ArrayList<Stock> stockProductos = new ArrayList<>();
-      
-      
-      try{
-      ConnectionManager connectionManager = new ConnectionManager();
-        
-      Connection con = connectionManager.getConnection();
-      
-          PreparedStatement ps;
-          ResultSet rs;
-          String sql;
-        
-          //sql = "SELECT stock.stock_id, productos.prod_nombre, talles.talle_nombre, colores.color_nombre , cantidad FROM stock, productos, talles, colores WHERE stock.producto_id = productos.prod_id AND stock.talle_id = talles.talle_id AND stock.color_id = colores.color_id";
-   
-
- sql ="SELECT producto_id, prod_nombre, stock.talle_id, stock.color_id, stock.cantidad FROM productos, stock WHERE prod_id = stock.producto_id";                  
-          ps=con.prepareStatement(sql);
-
-          rs=ps.executeQuery();
-          
-          while (rs.next()){
-      //producto_id talle_id color_id stock
-            Stock cantidad=new Stock();
-            cantidad.setStockID(rs.getInt("producto_id"));
-            cantidad.setNombreProducto(rs.getString("prod_nombre"));
-            cantidad.setTalleID(rs.getInt("stock.talle_id"));
-            cantidad.setColorID(rs.getInt("stock.color_id"));
-            cantidad.setCant(rs.getInt("stock.cantidad"));
-            
-            stockProductos.add(cantidad);
-            
-              
-          }
-      } catch (SQLException ex) {
-            System.out.println("Error al obtener stock");
-        }
-        
-        return stockProductos;
-    
-        
-  }  */      
   
     public ArrayList<Stock> getListado(){
       
@@ -83,11 +38,6 @@ public class StockDAO {
           ResultSet rs;
           String sql;
         
-          //sql = "SELECT stock.stock_id, productos.prod_nombre, talles.talle_nombre, colores.color_nombre , cantidad FROM stock, productos, talles, colores WHERE stock.producto_id = productos.prod_id AND stock.talle_id = talles.talle_id AND stock.color_id = colores.color_id";
-   
-
- //sql ="SELECT prod_nombre, stock.talle_id, stock.color_id, stock.cantidad FROM productos, stock WHERE stock.producto_id = 1 AND stock.talle_id = 2 ";
-       
   sql ="SELECT producto_id, prod_nombre, stock.talle_id, stock.color_id, stock.cantidad FROM productos, stock WHERE prod_id = stock.producto_id AND stock.talle_id = 2 AND stock.color_id = 4 ";         
          
           ps=con.prepareStatement(sql);
@@ -184,8 +134,7 @@ public class StockDAO {
         
 sql="SELECT producto_id, stock.talle_id, stock.color_id, colores.color_nombre, stock.cantidad FROM stock, colores WHERE " + idproducto + " = stock.producto_id AND stock.talle_id = " + idTalles + " AND stock.color_id = colores.color_id";      
  
- // sql="SELECT producto_id, stock.talle_id, stock.color_id, stock.cantidad FROM stock WHERE " + idproducto + "= stock.producto_id AND stock.talle_id = "+ idTalles;
-        
+     
           ps=con.prepareStatement(sql);
 
           rs=ps.executeQuery();
@@ -229,13 +178,7 @@ sql="SELECT producto_id, stock.talle_id, stock.color_id, colores.color_nombre, s
           String sql;
         
       
-   /*    
-  sql ="SELECT producto_id, prod_nombre, stock.talle_id, stock.color_id, stock.cantidad "
-          + " FROM productos, stock "
-          + "WHERE "+ idproducto + " = stock.producto_id " + " AND stock.talle_id = " + idTalles;        
-    */
-  //sql="SELECT producto_id, stock.talle_id, stock.color_id, stock.cantidad FROM stock WHERE " + idproducto + "= stock.producto_id AND stock.talle_id = "+ idTalles;
-  
+ 
   sql ="SELECT  stock.cantidad FROM stock WHERE " + idproducto + " = stock.producto_id AND stock.talle_id = " + idTalles  + " AND stock.color_id = "+ idColor;
 
           ps=con.prepareStatement(sql);
